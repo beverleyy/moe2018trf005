2ca7f373169292c8d7f697057027226f4487a1af
Beverley <bevswagmagnus@gmail.com>
2020-07-22 00:29:25 +0800

    Added comments in code for better documentation/updating/debugging

--- attendance-payment/parser.js
+++ attendance-payment/parser.js
@@ -1,40 +1,53 @@
-$(document).ready(function() {
-    $(".bar").each(function() {
+/* Form input effect script */
+// Honestly this is a little extra, but it looks nice, so...
+$(document).ready(function(){
+    // For each of the blanks
+    $(".bar").each(function(){
+        // Cache the variables
         var $bar = $(this),
             $highlight = $bar.next("span.input-highlight");
+        // Dupe the input text to the highlight, which changes width
         $highlight.html($bar.val());
-        $bar.on("input", function() {
+        $bar.on("input", function(){
             $highlight.html(this.value);
         });
     });
 });
-  
-function getData() {
+
+/* Grab the data from the Excel sheet and parse it */
+// Activates on submission of form (either hit Enter or click Submit)
+function getData(){
+    // Retrieve data from user inputs, trim off whitespace
     var matric = document.getElementById("matric").value.trim();
     var sheetID = document.getElementById("key").value.trim();
+    // Call Papa Parse on the Google spreadsheet
     Papa.parse('https://docs.google.com/spreadsheets/d/' + sheetID + '/pub?output=csv', {
-        download: true,
-        header: true,
-        complete: function(results) {
+        download: true, //this needs to be true for spreadsheet URL
+        header: true, //this needs to be true to sort the data later
+        complete: function(results){ //When parsing done:
             var bool = 0;
             var data = results.data;
-            for (var i = 0; i < data.length; i++) {
+            for (var i=0; i<data.length; i++) {
+                //Check if entered matric number matches the current student
                 if (data[i]["Matriculation Number"].trim().toLowerCase() == matric.toLowerCase()) {
-                    generateData(data[i]);
-                    bool = 1;
-                    break;
-                }
-            }
+                    generateData(data[i]); //Display the student's data
+                    bool = 1; //Turn off error message
+                    break; //Exit the loop
+                }//end if
+            }//end for
+            //Error message, if matric number returns no matches
             if (bool == 0) {
                 var printDiv = document.getElementById("calculator");
                 $(printDiv).fadeIn();
                 printDiv.innerHTML = "Error: Matric number not found. You are not a registered participant. Please try again or email <a href='mailto:MOE2018TRF005@gmail.com'>MOE2018TRF005@gmail.com</a>.";
             }
         } //end complete function
-    });
-}
+    }); //end Parse
+} //end getData
   
-function generateData(info) {
+/* Function to print out student data */
+// Takes 1 argument, the data output by the parser
+function generateData(info){
     var outDiv = document.getElementById("calculator");
     $(outDiv).fadeIn();
     outDiv.innerHTML = "<h2>Record for:</h2>"
