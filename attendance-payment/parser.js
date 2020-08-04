c4dfb9c98f86a30248d6a06a235c64248f48e77b
beverleyy <yeo.beverley@gmail.com>
2020-08-05 00:23:54 +0800

    Added error message for failed data retrieval

--- attendance-payment/parser.js
+++ attendance-payment/parser.js
@@ -20,9 +20,13 @@ function getData(){
     // Retrieve data from user inputs, trim off whitespace
     var matric = document.getElementById("matric").value.trim();
     var sheetID = document.getElementById("key").value.trim();
+
+    // Cache DOM elements as vars
     var printDiv = document.getElementById("calculator");
     var outDiv = document.getElementById("output");
     var loader = document.getElementsByClassName("swing")[0];
+
+    // Print loading animation
     fadein(printDiv,printDiv.style.display);
     fadein(loader,loader.style.display);
     outDiv.style.display = "none";
@@ -32,33 +36,42 @@ function getData(){
     Papa.parse('https://moe2018trf005cors.herokuapp.com/https://docs.google.com/spreadsheets/d/' + sheetID + '/pub?output=csv', {
         download: true, //this needs to be true for spreadsheet URL
         header: true, //this needs to be true to sort the data later
+        error: function(){ //if parsing failed
+            var errStr = "Error: Failed to retrieve data from file. Please ensure you have entered the correct key. Email <a href='mailto:MOE2018TRF005@gmail.com'>MOE2018TRF005@gmail.com</a> if the problem persists.";
+            errFunc(errStr,outDiv,loader);
+        },
         complete: function(results){ //When parsing done:
             var bool = 0;
             var data = results.data;
             for (var i=0; i<data.length; i++) {
                 //Check if entered matric number matches the current student
                 if (data[i]["Matriculation Number"].trim().toLowerCase() == matric.toLowerCase()) {
-                    generateData(data[i]); //Display the student's data
+                    generateData(data[i],outDiv,loader); //Display the student's data
                     bool = 1; //Turn off error message
                     break; //Exit the loop
                 }//end if
             }//end for
             //Error message, if matric number returns no matches
             if (bool == 0) {
-                outDiv.innerHTML = "Error: Matric number not found. You are not a registered participant. Please try again or email <a href='mailto:MOE2018TRF005@gmail.com'>MOE2018TRF005@gmail.com</a>.";
-                document.getElementsByClassName("swing")[0].style.display = "none";
-                fadein(outDiv,outDiv.style.display);
+                var errStr = "Error: Matric number not found. You are not a registered participant. Please try again or email <a href='mailto:MOE2018TRF005@gmail.com'>MOE2018TRF005@gmail.com</a>.";
+                errFunc(errStr,outDiv,loader);
             }
         } //end complete function
     }); //end Parse
 } //end getData
+
+/* Error message function */
+function errFunc(errMsg,outDiv,loader){
+    outDiv.innerHTML = errMsg;
+    loader.style.display = "none";
+    fadein(outDiv,outDiv.style.display);
+}
   
 /* Function to print out student data */
 // Takes 1 argument, the data output by the parser
 // Probably not the best way to populate the report 
 // but uh..... I was given an hour to write this whole program
-function generateData(info){
-    var outDiv = document.getElementById("output");
+function generateData(info,outDiv,loader){
     outDiv.innerHTML = "<h2>Record for:</h2>"
     outDiv.innerHTML += "<h3>" + info["Full Name"] + ", " + info["Matriculation Number"] + "</h3>";
     outDiv.innerHTML += "<h3>Total payment due: <b>$" + info.TOTAL + "</b></h3>";
@@ -88,6 +101,6 @@ function generateData(info){
     outDiv.innerHTML += "</p>";
     outDiv.innerHTML += "<p>(Y = attended, N = did not attend, N/A = not eligible)</p><hr>";
     outDiv.innerHTML += "<p>If there is any discrepancy, please email <a href='mailto:MOE2018TRF005@gmail.com'>MOE2018TRF005@gmail.com</a>.";
-    document.getElementsByClassName("swing")[0].style.display = "none";
+    loader.style.display = "none";
     fadein(outDiv,outDiv.style.display);
 }
