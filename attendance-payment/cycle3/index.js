6c56f674c0337f03244a18b2761a7ea7e65a0c36
beverleyy <yeo.beverley@gmail.com>
2021-01-19 15:06:32 +0800

    Fix key check logic in index.js

--- attendance-payment/cycle3/index.js
+++ attendance-payment/cycle3/index.js
@@ -1,9 +1,9 @@
-const { Pool } = require('pg');
+const {Pool} = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 const express = require('express');
@@ -20,79 +20,82 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
 const middlewares = [
-  layout(),
-  express.static(path.join(__dirname, "public")),
-  bodyParser.urlencoded({ extended: true }),
-  bodyParser.json()
+    layout(),
+    express.static(path.join(__dirname, "public")),
+    bodyParser.urlencoded({ extended: true }),
+    bodyParser.json()
 ];
+
 app.use(middlewares);
 
 const {check,validationResult,matchedData} = require("express-validator");
 
 app.get('/', (req,res) => {
-  res.render('contact',{
-    input:false,
-    output:false,
-    flag:0,
-    errors:false,
-  });
+    res.render('contact',{
+        input:false,
+        output:false,
+        flag:0,
+        errors:false,
+    });
 });
 
 app.post('/',[
-  check("matric")
-    .isLength({min:1})
-    .withMessage("Matric number is required")
-    .trim(),
-  check("key")
-    .isLength({min:1})
-    .withMessage("Key is required")
-    .trim()
-  ],
-  async (req,res) => {
-    try {
-      const errors = validationResult(req);
-      const matric_no = req.body.matric.trim().toUpperCase();
-      const sqlq = "SELECT * FROM payment_base WHERE matric = $1";
-      const client = await pool.connect();
-      const result = await client.query(sqlq,[matric_no]);
-      const results = {'results': (result) ? result.rows : null};
+    check("matric")
+        .isLength({min:1})
+        .withMessage("Matric number is required")
+        .trim(),
+    check("key")
+        .isLength({min:1})
+        .withMessage("Key is required")
+        .trim()
+    ],
+    async (req,res) => {
+        try {
+            const errors = validationResult(req);
+            const matric_no = req.body.matric.trim().toUpperCase();
+            const sqlq = "SELECT * FROM payment_base WHERE matric = $1";
+            const client = await pool.connect();
 
-      const searchKey = await client.query('SELECT key FROM key_table');
-      const keys = {'keys': (searchKey) ? searchKey.rows : null};
-      const theKey = (JSON.stringify(keys.keys[0].key)).replace(/\"/g, "");
+            const searchKey = await client.query('SELECT key FROM key_table');
+            const keys = {'keys': (searchKey) ? searchKey.rows : null};
+            const theKey = (JSON.stringify(keys.keys[0].key)).replace(/\"/g, "");
 
-      if (req.body.key.trim().toUpperCase() === theKey.toUpperCase()){
-        output = results.results[0];
-        if (output){
-          res.render('contact',{
-            input:matchedData(req),
-            output:output,
-            flag:0,
-            errors:errors.mapped()
-          });
-        } else {
-          res.render('contact',{
-            input:matchedData(req),
-            output:false,
-            flag:1,
-            errors:errors.mapped()
-          });
+            if (req.body.key.trim().toUpperCase() === theKey.toUpperCase()){
+                const result = await client.query(sqlq,[matric_no]);
+                const results = {'results': (result) ? result.rows : null};
+        
+                output = results.results[0];
+        
+                if (output){
+                    res.render('contact',{
+                        input:matchedData(req),
+                        output:output,
+                        flag:0,
+                        errors:errors.mapped()
+                    });
+                } else {
+                    res.render('contact',{
+                        input:matchedData(req),
+                        output:false,
+                        flag:1,
+                        errors:errors.mapped()
+                    });
+                }
+            }
+            else {
+                res.render('contact',{
+                    input:matchedData(req),
+                    output:false,
+                    flag:2,
+                    errors:errors.mapped()
+                });
+            }
+            //console.log(JSON.stringify(output));
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send(err);
         }
-      }
-      else {
-          res.render('contact',{
-            input:matchedData(req),
-            output:false,
-            flag:2,
-            errors:errors.mapped()
-          });
-      }
-      //console.log(JSON.stringify(output));
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send(err);
-    }
 });
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
